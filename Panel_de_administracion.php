6f8777a4ed0da2e0c5e4de591c8da681b5f6d2a2
Incógnita X <julian.cortes43@gmail.com>
2020-10-22 14:42:43 -0500

    Create Panel_de_administracion.php

---
+++ Panel_de_administracion.php
@@ -0,0 +1,9 @@
+<?php require_once('../include/Session.php');?>
+<?php require_once('../include/Functions.php');?>
+<?php echo AdminAreaAccess(); ?>
+<?php
+<h2 class="text-center">
+                    BIENVENIDO AL TABLERO DE ADMINISTRACIÓN 
+	  <span><a href="logout" class="btn btn-success" style="float: right;">SALIR</a><span>
+</h2>
+?>
